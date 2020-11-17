53eb68fd2610762b01174973417bec0b08d3b95d
Husam <husam.burhan@gmail.com>
2020-11-17 19:37:19 +0000

    test post

--- web/index.php
+++ web/index.php
@@ -1,29 +1,36 @@
 <?php
 
-require('../vendor/autoload.php');
+if( isset($_POST['username']) )
+{
+  echo $_POST['username'];
+  echo $_POST['password'];
+  
+}
+else{
+  require('../vendor/autoload.php');
 
-$app = new Silex\Application();
-$app['debug'] = true;
+  $app = new Silex\Application();
+  $app['debug'] = true;
 
-// Register the monolog logging service
-$app->register(new Silex\Provider\MonologServiceProvider(), array(
-  'monolog.logfile' => 'php://stderr',
-));
+  // Register the monolog logging service
+  $app->register(new Silex\Provider\MonologServiceProvider(), array(
+    'monolog.logfile' => 'php://stderr',
+  ));
 
-// Register view rendering
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/views',
-));
+  // Register view rendering
+  $app->register(new Silex\Provider\TwigServiceProvider(), array(
+      'twig.path' => __DIR__.'/views',
+  ));
 
-// Our web handlers
+  // Our web handlers
 
-$app->get('/', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return $app['twig']->render('index.twig');
-});
+  $app->get('/', function() use($app) {
+    $app['monolog']->addDebug('logging output.');
+    return $app['twig']->render('index.twig');
+  });
 
-$app->run();
-
+  $app->run();
+}
 /*$handle = fopen("data.txt", "r");
 if ($handle) {
     while (($line = fgets($handle)) !== false) {
